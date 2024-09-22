eec228ff325ef297a286d90e00d8bd01e475fbfa
Mathilde <math68n8@stud.kea.dk>
2024-09-22 14:47:49 +0200

    installed chakra

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+import * as React from 'react'
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
+
 function App() {
-  const [count, setCount] = useState(0)
-
+  // 2. Wrap ChakraProvider at the root of your app
   return (
-   <h1>Hello there!</h1>
-  );
+    <ChakraProvider>
+     <h1>Hello there</h1>
+    </ChakraProvider>
+  )
 }
-
 export default App
